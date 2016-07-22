0b30a1fbce3cee39ea6b4719c0a5f220c0dc4745
alihanniba <alihanniba@gmail.com>
2016-07-22 19:16:45 +0800

    add folder

---
+++ php/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @authors alihanniba          alihanniba@gmail.com
+ * @date    2016-07-22 19:15:48
+ * @version \www.alihanniba.com\
+ */
+
+class ClassName extends AnotherClass {
+    
+    function __construct(){
+
+    }
+}
